460cf85652a1ee61b7a28291972e32281f57a556
surajbhukebag <surajbhukebag@gmail.com>
2017-12-17 02:27:26 -0800

    Search API

---
+++ CSUR/src/main/java/com/cmpe275/project/CSUR/dao/StationDao.java
@@ -0,0 +1,9 @@
+package com.cmpe275.project.CSUR.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cmpe275.project.CSUR.model.Station;
+
+public interface StationDao extends CrudRepository<Station, Long> {
+
+}
--- CSUR/src/main/java/com/cmpe275/project/csur/controller/SearchController.java
+++ CSUR/src/main/java/com/cmpe275/project/csur/controller/SearchController.java
@@ -1,5 +1,52 @@
 package com.cmpe275.project.CSUR.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cmpe275.project.CSUR.mapper.SearchCriteria;
+import com.cmpe275.project.CSUR.mapper.SearchResults;
+import com.cmpe275.project.CSUR.services.SearchService;
+
+@RestController
 public class SearchController {
+
+	@Autowired
+	private SearchService searchService;
+
+	@GetMapping(path = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> searchTrains(
+			@RequestParam(value = "noOfPassengers", required = false) Integer noOfPassengers,
+			@RequestParam(value = "departureTime", required = false) String departureTime,
+			@RequestParam(value = "depDate", required = false) String depDate,
+			@RequestParam(value = "from", required = false) String from,
+			@RequestParam(value = "to", required = false) String to,
+			@RequestParam(value = "trainType", required = false) String trainType,
+			@RequestParam(value = "noOfConnections", required = false) Integer noOfConnections,
+			@RequestParam(value = "roundTrip", required = false) String roundTrip,
+			@RequestParam(value = "isExact", required = false) boolean isExact) {
+
+		SearchCriteria searchCriteria = new SearchCriteria();
+		searchCriteria.setDepDate(Long.valueOf(depDate));
+		searchCriteria.setExact(isExact);
+		searchCriteria.setNoOfPassengers(noOfPassengers);
+		searchCriteria.setNoOfConnections(noOfConnections);
+		searchCriteria.setDepartureTime(Long.valueOf(departureTime));
+		searchCriteria.setFrom(from);
+		searchCriteria.setTo(to);
+		searchCriteria.setTrainType(trainType);
+		searchCriteria.setRoundTrip(roundTrip);
+
+		List<SearchResults> results = searchService
+				.searchTrains(searchCriteria);
+
+		return new ResponseEntity(results, HttpStatus.OK);
+	}
 
 }
